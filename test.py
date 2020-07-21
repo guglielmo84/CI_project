813c815d3455b286cf64e35bb6d24c80be72bbad
andrea <andrealisi88@gmail.com>
2020-07-21 16:14:36 +0200

    mapping + modified test.py

--- test.py
+++ test.py
@@ -3,7 +3,11 @@ import json
 
 def enrichContent(data):
   res = dict()
+  res['geo'] = dict()
   for k,v in data.items():
+    if k.lower() in ['latitude', 'longitude']:
+      res['geo'][k[:3].lower()] = v
+      continue
     res[k.lower().replace(' ','_')] = v
   return res
 
@@ -13,7 +17,6 @@ for line in fileinput.input():
     # Parse JSON data.
     parsedJson = json.loads(line)
 
-    # Clean tweet text.
     enrichedData = enrichContent(parsedJson)
     if enrichedData is None:
         continue
