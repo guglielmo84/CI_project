3496da71297eb7430da4e8385b8eecd949822e82
andrea <andrealisi88@gmail.com>
2020-07-22 14:37:04 +0200

    update python files and mapping

--- conv_csv_json.py
+++ conv_csv_json.py
@@ -13,27 +13,10 @@ with open("PRC Data Breach Chronology - 1.13.20.csv", 'r') as f:
       _tmp=dict()
       for k,v in zip(head, l):
         if not k:
-         continue
-        if k == "Date Made Public":
-         tt = v.split('/')
-         b = datetime.date(int(tt[-1]), int(tt[0]) , int(tt[1]))
-         _tmp[k] = b.isoformat()
-         _tmp['date'] = b.isoformat()
-        elif k in ['Latitude','Longitude']:
-         try:
-           _tmp[k] = float(v)
-         except:
-           _tmp[k] = v
-        elif k in ['Total Records', "Year of Breach"]:
-         try:
-           _tmp[k] = int(v.replace(',',''))
-         except:
-           _tmp[k] = ''
-        else:
-         _tmp[k] = v.replace('\n', ' ')
+          continue
+        _tmp[k] = v
       with open(os.path.join(path, "{}.json".format(i)), "w+") as f:
         json.dump(_tmp,f, ensure_ascii=True)
       i += 1
   except Exception as e:
     print(e)
-    import ipdb;ipdb.set_trace()
--- script/data_formatter.py
+++ script/data_formatter.py
@@ -1,15 +1,36 @@
 import fileinput
 import json
+import datetime
 
-def enrichContent(data):
-  res = dict()
-  res['geo'] = dict()
-  for k,v in data.items():
-    if k.lower() in ['latitude', 'longitude']:
-      res['geo'][k[:3].lower()] = v
-      continue
-    res[k.lower().replace(' ','_')] = v
-  return res
+#clean data
+def format_data(data):
+  keys = data.keys()
+  _tmp = dict()
+  # create custom object for geo data
+  _tmp['geo'] = dict()
+  for k in keys:
+    v = data[k]
+    # delete space from json keys
+    kk = k.lower().replace(' ','_')
+    if k == "Date Made Public":
+      # format data to ISO format
+      tt = v.split('/')
+      b = datetime.date(int(tt[-1]), int(tt[0]) , int(tt[1]))
+      _tmp[kk] = b.isoformat()
+      _tmp['date'] = b.isoformat()
+    elif k in ['Latitude','Longitude']:
+      try:
+       _tmp['geo'][kk[:3].lower()] = float(v)
+      except:
+       _tmp['geo'][kk[:3].lower()] = v
+    elif k in ['Total Records', "Year of Breach"]:
+      try:
+       _tmp[kk] = int(v.replace(',',''))
+      except:
+       _tmp[kk] = ''
+    else:
+      _tmp[kk] = v.replace('\n', ' ').replace('\u00a0', '')
+  return _tmp
 
 # Read all data coming from stdin. Infinite loop.
 for line in fileinput.input():
@@ -17,7 +38,7 @@ for line in fileinput.input():
     # Parse JSON data.
     parsedJson = json.loads(line)
 
-    enrichedData = enrichContent(parsedJson)
+    enrichedData = format_data(parsedJson)
     if enrichedData is None:
         continue
 
